61d8d85bf84d0971c8642e3038c4502ca9958759
layfieldk <kevinjlayfield@gmail.com>
2015-08-24 00:13:45 +0000

    Attempt to eliminate postgresql problem regarding the counts of tags in production

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,6 +5,6 @@ class Tag < ActiveRecord::Base
 
   
   def self.counts
-    self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+    self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id, tags.id, tags.name")
   end
 end
